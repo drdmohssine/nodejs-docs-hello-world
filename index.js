d3f061a547f0415f2e3b224686535b5279ed10cd
Mouhcine Dardour <mohssine-d@hotmail.com>
2018-01-11 09:54:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! MY first app ");
+    response.end("Hello World! Test Jenkins & Docker ");
     
 });
 
